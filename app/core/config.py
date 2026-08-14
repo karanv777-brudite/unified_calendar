458a556b298ffe974c2e31e6d86aa5ebe39b0d96
Karan <karanverma55777@gmail.com>
2026-08-14 17:34:25 +0530

    Fixed the Encryption bug

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    # It automatically searches for .env and fetches the sensitive data from the file., And if not found then replaces them with the default placeholder.
     GOOGLE_CLIENT_ID: str = "YOUR_GOOGLE_CLIENT_ID"
     GOOGLE_CLIENT_SECRET: str = "YOUR_GOOGLE_CLIENT_SECRET"
     GOOGLE_REDIRECT_URI: str = "http://localhost:8000/auth/google/callback"
@@ -11,7 +12,7 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str = "postgresql+asyncpg://postgres:password@localhost:5432/calendar_db"
 
-    ENCRYPTION_KEY: str = "1234"
+    ENCRYPTION_KEY: str = "Random_Key=1234"
 
     model_config = SettingsConfigDict(
         env_file=".env",
